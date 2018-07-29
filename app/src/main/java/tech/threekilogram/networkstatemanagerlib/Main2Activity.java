a4c565ce60dbce3e22ff459ba7ac094d9d83a34a
liujin <wuxiongc@163.com>
2018-07-29 13:16:54 +0800

    当添加监听时回调一次

--- app/src/main/java/tech/threekilogram/networkstatemanagerlib/Main2Activity.java
+++ app/src/main/java/tech/threekilogram/networkstatemanagerlib/Main2Activity.java
@@ -5,12 +5,14 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.view.View;
+import android.widget.Toast;
 import tech.threekilogram.network.state.manager.NetStateChangeManager;
+import tech.threekilogram.network.state.manager.OnNetStateChangedListener;
 
 /**
  * @author liujin
  */
-public class Main2Activity extends AppCompatActivity {
+public class Main2Activity extends AppCompatActivity implements OnNetStateChangedListener {
 
       public static void start (Context context) {
 
@@ -25,8 +27,28 @@ public class Main2Activity extends AppCompatActivity {
             setContentView(R.layout.activity_main2);
       }
 
+      @Override
+      protected void onResume () {
+
+            super.onResume();
+            NetStateChangeManager.getInstance().addListener(this);
+      }
+
+      @Override
+      protected void onPause () {
+
+            super.onPause();
+            NetStateChangeManager.getInstance().removeListener(this);
+      }
+
       public void unRegister (View view) {
 
             NetStateChangeManager.unRegisterReceiver(this);
+      }
+
+      @Override
+      public void onNetWorkStateChanged (int state) {
+
+            Toast.makeText(this, "当前网络状态" + state, Toast.LENGTH_SHORT).show();
       }
 }
